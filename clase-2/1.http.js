8d2058e14694b3596fa765e924e8b187ead7e3c7
Daniel Ortiz <andresvertel2021@gmail.com>
2024-02-21 09:24:06 -0500

    nodemon

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -2,10 +2,25 @@ const http = require('node:http');
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-    console.log('request received')
-    res.end('hello world')
-})
+const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+   
+
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end('Bienvenido a la página principal')
+  }else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.end('Contacto')
+  }else {
+    res.statusCode = 404
+    res.end('Página no encontrada')
+  }
+}
+
+
+const server = http.createServer(processRequest)
+
 
 server.listen(desiredPort, () => {
     console.log(`server listening on port http://localhost:${desiredPort}`)
