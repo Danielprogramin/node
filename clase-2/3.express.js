14650f0f707bec6a2562c0211fd0101359b22a04
Daniel Ortiz <andresvertel2021@gmail.com>
2024-06-04 14:58:19 -0500

    viejo

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,10 +1,35 @@
 const esxpress = require('express')
 const app = esxpress()
+const ditto = require('./pokemon/ditto.json')
 
-const PORT = process.env.PORT ?? 1234
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Mi pagina</h1>')
+
+app.disable('x-powered-by')
+
+app.use((res, req, next) => {
+    if(req.method === 'POST') return next()
+
+   if(res.setHeader['Content-Type'] === 'application/json; charset=utf-8') return next()
+
+   let body = ''
+
+   // Evento de lectura de datos
+   req.on('data', chunk => {
+       body += chunk.toString()
+   })
+
+   req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    //mutar el body
+   req.body = data
+})
+
+    next()
+})
+
+app.get('/pokemon/ditto', (req, res) => {
+    res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
@@ -21,6 +46,10 @@ app.post('/pokemon', (req, res) => {
         data.timestamp = Date.now()
         res.status(201).json(data)
     })
+})
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>')
 })
 
 app.listen(PORT, () => {
