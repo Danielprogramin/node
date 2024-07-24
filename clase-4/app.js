bf1193d66379642f66fb78fa953ae263619928e6
Daniel Ortiz <programador12@tecnologicocomfenalco.edu.co>
2024-07-24 11:25:19 -0500

    routes

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,7 +1,7 @@
 import express, { json } from 'express'
 import { randomUUID } from 'node:crypto'
 
-import { validateMovie, validatePartialMovie } from './schemas/movies.js'
+import { moviesRouter } from './routes/movies.js'
 
 //como leer un json en ESModules 
 //import fs from 'node:fs'
@@ -22,99 +22,7 @@ const ACCEPTED_ORIGINS = [
     'https://Daniel.dev'
 ]
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello'})
-})
-
-app.get('/movies', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-
-
-
-    const { genre} = req.query
-    if (genre){
-        const filtereMovies = movies.filter(
-            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-        )
-        return res.json(filtereMovies)
-    }
-    res.json(movies)
-})
-
-app.get('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movie = movies.find(movie => id === movie.id)
-    if (movie) return res.json(movie)
-
-        res.status(404).json({ message: 'Movie not found' })
-})
-
-app.post('/movies', (req, res) => {
-
-    const result = validateMovie(req.body)
-
-    if (!result.success) {
-        return res.status(404).json({ error: JSON.parse(result.error.message)})
-    }
-
-    // en base de datos 
-    const newMovie = {
-        id: randomUUID(), // uuid v4
-        ...result.data // data
-    }
-
-    //esto no es REST, porque estamos guardadrdo
-    //el estadoo de la aplicacion en memoria 
-
-    movies.push(newMovie)
-
-    res.status(201).json(newMovie)
-})
-
-app.delete('/movies/:id', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: 'Movie not found'})
-    }
-
-    movies.splice(movieIndex, 1)
-
-    return res.json({ message: 'Movie deleted'})
-})
-
-app.patch('/movies/:id', (req, res) => {
-
-    const result = validatePartialMovie(req.body)
-    
-    if (!result.success) {
-        return res.status(404).json({ error: JSON.parse(result.error.message)})
-    }
-
-    const {id} = req.params
-    const movieIndex = movies.findIndex(movie = movie.id === id )
-
-    if (movieIndex === -1){
-        return res.status(404).json({ message: 'Movie not found'})
-    }
-
-    const updateMovie = {
-        ...movies[movieIndex],
-        ...result.data
-    }
-    
-    movies[movieIndex] = updateMovie
-
-    return res.json(updateMovie)
-})
+app.use('/movies' , moviesRouter)
 
 app.options('/movies/:id', (req, res) => {
     const origin = req.header('origin')
--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -1,10 +1,13 @@
+import { randomUUID } from 'node:crypto'
 import { Router } from 'express';
 import {readJSON} from './utils.js'
 
-const movies = readJSON('./movies.json')
-const router = Router()
+import { validateMovie, validatePartialMovie } from './schemas/movies.js'
 
-router.get('/', (req, res) => {
+const movies = readJSON('./movies.json')
+export const moviesRouter = Router()
+
+moviesRouter.get('/', (req, res) => {
     const { genre} = req.query
     if (genre){
         const filtereMovies = movies.filter(
@@ -13,4 +16,75 @@ router.get('/', (req, res) => {
         return res.json(filtereMovies)
     }   
     res.json(movies)
+})
+
+moviesRouter.get('/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+        res.status(404).json({ message: 'Movie not found'})
+})
+
+moviesRouter.post('/' , (req, res) => {
+
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        return res.status(404).json({ error: JSON.parse(result.error.message)})
+    }
+
+    // en base de datos 
+    const newMovie = {
+        id: randomUUID(), // uuid v4
+        ...result.data // data
+    }
+
+    //esto no es REST, porque estamos guardadrdo
+    //el estadoo de la aplicacion en memoria 
+
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
+})
+
+moviesRouter.delete('/:id', (req, res) => {
+    const origin = req.header('origin')
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
+        res.header('Access-Control-Allow-Origin', origin)
+    }
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found'})
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted'})
+})
+
+moviesRouter.patch('/:id', (req, res) => {
+
+    const result = validatePartialMovie(req.body)
+    
+    if (!result.success) {
+        return res.status(404).json({ error: JSON.parse(result.error.message)})
+    }
+
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie = movie.id === id )
+
+    if (movieIndex === -1){
+        return res.status(404).json({ message: 'Movie not found'})
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+    
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
 })
